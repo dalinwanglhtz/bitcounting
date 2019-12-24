1b9f413fdd64bb4dcb9d8298d47362878f5c01f7
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-24 23:00:22 +1100

    first working cut

--- src/com/codewardev/BitCounting.java
+++ src/com/codewardev/BitCounting.java
@@ -1,10 +1,26 @@
 package com.codewardev;
 
+// https://www.codewars.com/kata/526571aae218b8ee490006f4/train/java
+
 public class BitCounting {
 
 	public static int countBits(int n) {
-		// TODO Auto-generated method stub
-		return -1;
+		
+		int remainder;
+		long binaryVal=0;
+		int numOnes=0;
+		long j=1;
+		while(n != 0) {
+			remainder = n % 2;
+			binaryVal += remainder * j;
+			j *= 10;
+			n /= 2;
+			if(remainder==1) numOnes++;
+		}
+		
+		System.out.println("Binary: "+binaryVal);
+		
+		return numOnes;
 	}
 
 }
